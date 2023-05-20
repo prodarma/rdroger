639b192a711fc0e4ac6198549c86dace40fdaae2
Arma <armaproton@protonmail.com>
2023-05-20 05:45:50 +0200

    full website

--- loader.js
+++ loader.js
@@ -1,8 +1,15 @@
 const loader = document.querySelector(".loader");
 
-document.querySelector(".loader")
-setInterval(closeloader, 5000);
-
 function closeloader() {
     loader.style.display = "none";
+}
+
+let alreadySeen = false
+if (localStorage.getItem("alreadySeen") == "true") {
+    loader.style.display = "none";
+}
+else {
+    document.querySelector(".loader")
+    setInterval(closeloader, 5000);
+    localStorage.setItem("alreadySeen", "true");
 }
